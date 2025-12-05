6570a17a33dba81399afcc01aa876185903c7b0c
Logali-btp2 <btp2@logaligroup.com>
2025-12-05 19:17:23 +0000

    CI/CD 2025

---
+++ project_fiori/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["logaligroup/projectfiori/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ project_fiori/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"logaligroup/projectfiori/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
